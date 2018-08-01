df74b55431cd7dcbeff330c5b2a80fcd08a0c0c8
myspace-nu <32058020+myspace-nu@users.noreply.github.com>
2018-08-01 12:52:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# MySpace.SerialCommunication
+
+Making serial communication a bit easier.
+
+## Installation
+
+Add a reference to the class .dll in your project. For convenience, also add the namespace.
+
+	using MySpace.SerialCommunication;
+	
+## Usage
+
+	SerialCommunication mySerial;
+	// ...
+	mySerial = new SerialCommunication(serialPort1); // Create a new instance and bind it to serialPort1
+	mySerial.port.BaudRate = 57600; // Set port properties
+	mySerial.onDataReceived = () =>
+	{
+		// Optional event for continuously monitoring incoming data.
+		// string mySerial.buffer
+		// byte[] mySerial.bufferBytes
+	};
+	// ...
+	mySerial.port.PortName = "COM1";
+	try { mySerial.port.Open();  }
+	catch
+	{
+		// Unable to open port. It might be in use by another application.
+	}
+	//
+	sCmd = new SerialCommunication.serialCommand(mySerial);
+	sCmd.command = "ATI\r\n";
+	sCmd.waitAfter = "OK";
+	sCmd.onSuccess = () =>
+	{
+		MessageBox.Show(sCmd.instance.response.text);
+	};
+	sCmd.onFail = () =>
+	{
+		MessageBox.Show("Serial device did not respond in a timely manner.");
+	};
+	sCmd.onComplete = () =>
+	{
+		MessageBox.Show("All done.");
+	};
+	mySerial.commands.Add(sCmd);
+
+## SerialCommunication class
+
+**commands** - List of serial commands (*List<SerialCommunication.serialCommand>*)
+
+**response** - Serial response (*SerialCommunication.serialResponse*)
+
+**responseTimeout** - Default response timeout for commands (*long*)
+
+*Default: 2000*
+
+**sendDelay** - Wait this many milliseconds between bytes when sending data on the serial port (*long*)
+
+*Default: 5*
+
+**buffer** - Serial data buffer (*string*)
+
+**bufferBytes** - Serial data buffer (*byte[]*)
+
+**port** - Serial port object (*System.IO.Ports.SeriaPort*)
+
+**onDataReceived** - Event for monitoring incoming serial data (*event*)
+
+## SerialCommunication.serialCommand class
+
+**command** - Command to send (*string*)
+
+**commandBytes** - Command to send (*byte[]*)
+
+**waitBefore** - Wait for this string before sending command (*string*)
+
+**waitAfter** - Wait for this string after the command has been sent (*string*)
+
+**waitAfterMS** - Wait this many milliseconds after the command has been sent (*long*)
+
+**waitAfterBytes** - Wait this many bytes to be received after the command has been sent (*long*)
+
+**responseTimeout** - The command will timeout and fail after this many milliseconds (*long*)
+
+*Default: SerialCommunication.responseTimeout*
+
+**onSuccess** - Event to execute on success (*event*)
+
+**onFail** - Event to execute on fail (*event*)
+
+**onComplete** - Event to execute on success or fail (*event*)
+
+**instance** - Instance of SerialCommunication
+
+## SerialCommunication.serialResponse class
+
+**text** - The response as text (*string*)
+
+**bytes** - The response as byte array (*byte[]*)
+
+**responseTime** - The time in milliseconds it took to for the serial command to complete. (*long*)
---
+++ SerialCommunication/MySpace.SerialCommunication.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace MySpace.SerialCommunication
+{
+    public class SerialCommunication
+    {
+        public SerialCommunication(SerialPort p)
+        {
+            port = p;
+            commands = new List<serialCommand>();
+            response = new serialResponse(this);
+            sendDelay = 5;
+            responseTimeout = 2000;
+            serialTimer.Elapsed += new System.Timers.ElapsedEventHandler(onTimedEvent);
+            serialTimer.Interval = 100;
+            serialTimer.Enabled = true;
+            onDataReceived = () => {
+                // Do something
+            };
+        }
+        private List<serialCommand> _commands;
+        public List<serialCommand> commands
+        {
+            get { return _commands; }
+            set { _commands = value; response = new serialResponse(this); }
+        }
+        public System.Timers.Timer serialTimer = new System.Timers.Timer();
+        public serialResponse response;
+        public DateTime lastCommandSent;
+        public int sendDelay;
+        public int responseTimeout;
+        public byte[] bufferBytes;
+        public string buffer;
+        private SerialPort _port;
+        public SerialPort port
+        {
+            get { return _port; }
+            set
+            {
+                _port = value;
+                _port.BaudRate = 57600;
+                _port.DataBits = 8;
+                _port.Parity = Parity.None;
+                _port.StopBits = StopBits.One;
+                _port.WriteTimeout = 500;
+                _port.ReadTimeout = 500;
+                _port.Handshake = Handshake.None;
+                _port.RtsEnable = false;
+                _port.DtrEnable = false;
+                _port.NewLine = "\r\n";
+                _port.DataReceived += new SerialDataReceivedEventHandler(onSerialDataReceived);
+            }
+        }
+        public Action onDataReceived { get; set; }
+        // Events
+        private void onSerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            bufferBytes = new byte[port.BytesToRead];
+            port.Read(bufferBytes, 0, bufferBytes.Length);
+            buffer = System.Text.Encoding.Default.GetString(bufferBytes);
+            response.bytes = ByteArrayCombine(response.bytes, bufferBytes);
+            onDataReceived();
+        }
+        private void onTimedEvent(object source, System.Timers.ElapsedEventArgs e)
+        {
+            serialTimer.Stop();
+            serialTimer.Enabled = false;
+            if (port.IsOpen && commands.Count > 0)
+            {
+                process();
+            }
+            serialTimer.Enabled = true;
+            serialTimer.Start();
+        }
+        // Classes
+        public class serialCommand
+        {
+            public SerialCommunication instance;
+            public serialCommand(SerialCommunication instance)
+            {
+                this.instance = instance;
+                isCommandSent = false;
+                responseTimeout = instance.responseTimeout;
+                waitBeforeCopy = null;
+                waitAfterCopy = null;
+                waitAfterMSCopy = null;
+                waitAfterBytesCopy = null;
+                onSuccess = () => {
+                    // System.Diagnostics.Debugger.Log(0, null, "Success:" + instance.response.text);
+                };
+                onFail = () => {
+                    // System.Diagnostics.Debugger.Log(0, null, "Fail:" + instance.response.text);
+                };
+                onComplete = () => {
+                    // System.Diagnostics.Debugger.Log(0, null, "Complete:" + instance.response.text);
+                };
+
+            }
+            public int responseTimeout;
+            public string command { get; set; }
+            public byte[] commandBytes { get; set; }
+            public bool isCommandSent { get; set; }
+            string _waitBefore;
+            public string waitBefore
+            {
+                get { return _waitBefore; }
+                set { _waitBefore = value; if (waitBeforeCopy == null) { waitBeforeCopy = _waitBefore; } }
+            }
+            string _waitAfter;
+            public string waitAfter
+            {
+                get { return _waitAfter; }
+                set { _waitAfter = value; if (waitAfterCopy == null) { waitAfterCopy = _waitAfter; } }
+            }
+            long _waitAfterMS;
+            public long waitAfterMS
+            {
+                get { return _waitAfterMS; }
+                set { _waitAfterMS = value; if (waitAfterMSCopy == null) { waitAfterMSCopy = _waitAfterMS; } }
+            }
+            long _waitAfterBytes;
+            public long waitAfterBytes
+            {
+                get { return _waitAfterBytes; }
+                set { _waitAfterBytes = value; if (waitAfterBytesCopy == null) { waitAfterBytesCopy = _waitAfterBytes; } }
+            }
+            public Action onSuccess { get; set; }
+            public Action onFail { get; set; }
+            public Action onComplete { get; set; }
+
+            // Copy of properties
+            private string waitBeforeCopy { get; set; }
+            private string waitAfterCopy { get; set; }
+            private long? waitAfterMSCopy { get; set; }
+            private long? waitAfterBytesCopy { get; set; }
+            public void reset()
+            {
+                waitBefore = waitBeforeCopy;
+                waitAfter = waitAfterCopy;
+                waitAfterMS = waitAfterMSCopy ?? default(long);
+                waitAfterBytes = waitAfterBytesCopy ?? default(long);
+                isCommandSent = false;
+            }
+        }
+        public class serialResponse
+        {
+            private SerialCommunication instance;
+            public serialResponse(SerialCommunication instance)
+            {
+                this.instance = instance;
+                clear();
+            }
+            public void clear()
+            {
+                text = string.Empty;
+                bytes = new byte[] { };
+            }
+            // public string command { get; set; }
+            public string text { get; set; }
+            private byte[] _bytes;
+            public byte[] bytes
+            {
+                get { return _bytes; }
+                set
+                {
+                    _bytes = value;
+                    text = System.Text.Encoding.Default.GetString(bytes);
+
+                }
+            }
+
+            private long _responseTime;
+            public long responseTime
+            {
+                get { return (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) - (instance.lastCommandSent.Ticks / TimeSpan.TicksPerMillisecond); }
+                set
+                {
+                    _responseTime = value;
+                }
+            }
+        }
+        // Functions
+        public void process()
+        {
+            // Wait before command
+            if (!this.commands[0].isCommandSent && !string.IsNullOrEmpty(this.commands[0].waitBefore))
+            {
+                Regex waitRegExp = new Regex(this.commands[0].waitBefore, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                if (waitRegExp.IsMatch(this.response.text))
+                {
+                    this.commands[0].waitBefore = null;
+                }
+
+            }
+            // Send command
+            if (!this.commands[0].isCommandSent && string.IsNullOrEmpty(this.commands[0].waitBefore))
+            {
+                bool portSendFailure = false;
+                this.response.clear();
+                if (this.commands[0].commandBytes != null && this.commands[0].commandBytes.Length > 0)
+                {
+                    for (int i = 0; i < this.commands[0].commandBytes.Length && !portSendFailure; i++)
+                    {
+                        try { this.port.Write(this.commands[0].commandBytes, i, 1); }
+                        catch { portSendFailure = true; }
+                        WinApi.TimeBeginPeriod(1);
+                        System.Threading.Thread.Sleep(this.sendDelay);
+                        WinApi.TimeEndPeriod(1);
+                    }
+                }
+                else
+                {
+                    foreach (char c in this.commands[0].command)
+                    {
+                        try { this.port.Write(c.ToString()); }
+                        catch { portSendFailure = true; }
+                        WinApi.TimeBeginPeriod(1);
+                        System.Threading.Thread.Sleep(this.sendDelay);
+                        WinApi.TimeEndPeriod(1);
+                    }
+                }
+                this.commands[0].isCommandSent = true;
+                this.lastCommandSent = DateTime.Now;
+            }
+            // Wait after command - Regex
+            if (this.commands[0].isCommandSent && !string.IsNullOrEmpty(this.commands[0].waitAfter))
+            {
+                Regex waitRegExp = new Regex(this.commands[0].waitAfter, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                if (waitRegExp.IsMatch(this.response.text))
+                {
+                    this.commands[0].waitAfter = null;
+                }
+            }
+            // Wait after command - Time
+            if (this.commands[0].isCommandSent && (this.commands[0].waitAfterMS > 0))
+            {
+                if (this.response.responseTime > this.commands[0].waitAfterMS)
+                {
+                    this.commands[0].waitAfterMS = 0;
+                }
+            }
+            // Wait after command - Number of bytes
+            if (this.commands[0].isCommandSent && (this.commands[0].waitAfterBytes > 0))
+            {
+                if (this.response.bytes.Length >= this.commands[0].waitAfterBytes)
+                {
+                    this.commands[0].waitAfterBytes = 0;
+                }
+            }
+            // Command completed successfully
+            if (this.commands[0].isCommandSent &&
+                string.IsNullOrEmpty(this.commands[0].waitBefore) &&
+                string.IsNullOrEmpty(this.commands[0].waitAfter) &&
+                !(this.commands[0].waitAfterMS > 0) &&
+                !(this.commands[0].waitAfterBytes > 0))
+            {
+                this.commands[0].onSuccess();
+                this.commands[0].onComplete();
+                this.commands.RemoveAt(0);
+
+            }
+            // Command failed - Timeout
+            else if (this.commands[0].isCommandSent && this.response.responseTime > this.commands[0].responseTimeout)
+            {
+                this.commands[0].onFail();
+                this.commands[0].onComplete();
+                this.commands.RemoveAt(0);
+            }
+
+        }
+        // Generic functions
+        public static byte[] ByteArrayCombine(params byte[][] arrays)
+        {
+            int len = 0;
+            foreach (byte[] array in arrays) { if (array != null) len += array.Length; }
+            // byte[] rv = new byte[arrays.Sum(a => a.Length)];
+            byte[] rv = new byte[len];
+            int offset = 0;
+            foreach (byte[] array in arrays)
+            {
+                if (array != null)
+                {
+                    System.Buffer.BlockCopy(array, 0, rv, offset, array.Length);
+                    offset += array.Length;
+                }
+            }
+            return rv;
+        }
+        public static byte[] ByteArrayLast(byte[] ByteArray, int Length)
+        {
+            byte[] rv = new byte[Math.Min(ByteArray.Length, Length)];
+            System.Buffer.BlockCopy(ByteArray, Math.Max(ByteArray.Length - Length, 0), rv, 0, rv.Length);
+            return rv;
+        }
+        public static byte[] ByteArrayFirst(byte[] ByteArray, int Length)
+        {
+            byte[] rv = new byte[Math.Min(ByteArray.Length, Length)];
+            System.Buffer.BlockCopy(ByteArray, 0, rv, 0, rv.Length);
+            return rv;
+        }
+    }
+    public static class WinApi
+    {
+        /// <summary>TimeBeginPeriod(). See the Windows API documentation for details.</summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2118:ReviewSuppressUnmanagedCodeSecurityUsage"), System.Security.SuppressUnmanagedCodeSecurity]
+        [System.Runtime.InteropServices.DllImport("winmm.dll", EntryPoint = "timeBeginPeriod", SetLastError = true)]
+        public static extern uint TimeBeginPeriod(uint uMilliseconds);
+        /// <summary>TimeEndPeriod(). See the Windows API documentation for details.</summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2118:ReviewSuppressUnmanagedCodeSecurityUsage"), System.Security.SuppressUnmanagedCodeSecurity]
+        [System.Runtime.InteropServices.DllImport("winmm.dll", EntryPoint = "timeEndPeriod", SetLastError = true)]
+        public static extern uint TimeEndPeriod(uint uMilliseconds);
+    }
+}
